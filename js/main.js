f4d608aad2aba0b07962f88087c9f1f6579991c9
Broccoli <zeratukin@gmail.com>
2021-06-11 13:26:53 +1000

    vue

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /// <reference path="./jquery-3.6.0.js" />
 /// <reference path="./jquery-3.6.0.min.js" />
+
+Vue.createApp({
+    data() {
+        return {
+            name: "В разработке"
+        }
+    }
+}).mount('#app');
